a590b1dcf26c6b78ba664bce98e54b4e28366ed4
Danah Albaaj <dana.al.baaj@gmail.com>
2021-04-30 08:18:12 -0700

    Included KNN

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 
+
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import Binarizer
 data = pd.read_csv(r'C:\Users\danah\Documents\SeniorThesis\diabetes.csv')
 data.head()
 
 # statistical summary and distribution
+
 print('The statistical summary of the data is: \n', data.describe())
 print('The data distribution is: ', data.groupby('Outcome').size())
 
@@ -15,30 +19,31 @@ data.plot(kind='box', subplots = True, layout = (3,3), sharex = False, sharey = 
 data.hist()
 plt.show()
 
+# need to encode the pregnancy column, if > 1 change value to 1
+# this can be done using binarizer!
+binarizer = Binarizer(threshold = 1)
+binarizer.transform(np.array(data.loc[:,'Pregnancies']).reshape(-1,1))
+#
+data.hist()
+
 # This tells us where there are 0 values in the data
 # based on prior knowledge we can determine what 
 # is realistic and what should not be zero
 (data != 0).sum(0)
 
-# the number of zero values that should not exist in the dataset are located here
-data[data.Glucose == 0].shape[0]
-data[data.BloodPressure == 0].shape[0]
-data[data.Insulin == 0].shape[0]
-data[data.BMI == 0].shape[0]
-data[data.SkinThickness == 0].shape[0]
 
-# imputation -- replace these 0's with the median of the row 
-data.Glucose.replace(0, np.median(data.Glucose), inplace = True)
-data.Insulin.replace(0, np.median(data.Insulin), inplace = True)
-data.BloodPressure.replace(0, np.median(data.BloodPressure), inplace = True)
-data.SkinThickness.replace(0, np.median(data.SkinThickness), inplace = True)
-data.BMI.replace(0, np.median(data.BMI), inplace = True)
+# imputation -- replace the 0's and outliers with np.nan
 
-# replot the histograms to show the new distributions
-(data != 0).sum(0)
+columns = data.columns[1:6]
+for i in columns:
+	data.loc[:,i].replace(0, np.nan, inplace = True)
 
-# need to encode the pregnancy column, if > 1 change value to 1
-data.Pregnancies = np.where(data.Pregnancies != 0, 1, data.Pregnancies)
+# define KNN Imputer
+imputer = KNNImputer(n_neighbors = 5, weights = 'uniform', metric = 'nan_euclidean')
+X = data.drop(columns = ['Outcome'])
+imputer.fit(X)
+Xtrans = imputer.transform(X)
 
-# will need to build imputing into pipeline
-# must first separate into training and testing set in order to prevent any bias and other issues
+# need to use KNN to impute the missing values 
+# determine what values of insulin are biologically impossible and treat those as missing and impute
+# do not discard outliers
