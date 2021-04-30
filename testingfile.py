00061cff4d625c63e80443c841b72df7baa12663
Danah Albaaj <dana.al.baaj@gmail.com>
2021-04-30 08:17:50 -0700

    Reworked model

--- testingfile.py
+++ testingfile.py
@@ -1,55 +1,59 @@
-
-# First XGBoost model for Pima Indians dataset
 import numpy as np
 import pandas as pd
-import random
 import matplotlib.pyplot as plt
+import xgboost as xgb
+from xgboost import XGBClassifier
 %matplotlib inline
 
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import FunctionTransformer
+# from sklearn.compose import ColumnTransformer
+# from sklearn.compose import make_column_selector
+
+from sklearn.preprocessing import Binarizer
+from sklearn.impute import KNNImputer
 from sklearn.model_selection import train_test_split
-from numpy import loadtxt
-from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import Pipeline
+
 
 # load data
 data = pd.read_csv(r'C:\Users\danah\Documents\SeniorThesis\diabetes.csv')
 # split data into X and y
-X = data.drop(columns = ["Outcome"])
-Y = data['Outcome'].to_numpy()
+data.Pregnancies = np.where(data.Pregnancies != 0, 1, data.Pregnancies)
+X = data.drop(columns = ['Outcome'])
+y = data['Outcome'].to_numpy()
+
+
+missing_vals_ix = X.columns[1:6]
+
+for i in missing_vals_ix:
+    X.loc[:, i].replace(0, np.nan, inplace = True)
+
+imputer = KNNImputer(weights='distance')
+model = xgb.XGBClassifier(objective = 'binary:logistic', eval_metric = 'auc', use_label_encoder=False, learning_rate = 0.1, n_estimators = 10, max_depth = 5, alpha = 10)
+
+
+clf = Pipeline(steps=[('imputer', imputer), ('XGBoost', model)])
+
+
 # split data into train and test sets
 seed = 7
-test_size = 0.33
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+test_size = 0.3
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size, random_state = seed)
+# need to do cross validation up here, clf should represent the pipeline
+# need to build the cv into the pipeline
+# so the split of the training and testing happens multiple times!
 
-# Preprocess the data 
-# imputation -- replace these 0's with the median of the row 
-X_train.Glucose.replace(0, np.median(X_train.Glucose), inplace = True)
-X_train.Insulin.replace(0, np.median(X_train.Insulin), inplace = True)
-X_train.BloodPressure.replace(0, np.median(X_train.BloodPressure), inplace = True)
-X_train.SkinThickness.replace(0, np.median(X_train.SkinThickness), inplace = True)
-X_train.BMI.replace(0, np.median(X_train.BMI), inplace = True)
-X_test.Glucose.replace(0, np.median(X_test.Glucose), inplace = True)
-X_test.Insulin.replace(0, np.median(X_test.Insulin), inplace = True)
-X_test.BloodPressure.replace(0, np.median(X_test.BloodPressure), inplace = True)
-X_test.SkinThickness.replace(0, np.median(X_test.SkinThickness), inplace = True)
-X_test.BMI.replace(0, np.median(X_test.BMI), inplace = True)
-
-# need to encode the pregnancy column, if > 1 change value to 1
-X_train.loc[X_train['Pregnancies'] != 0, 'Pregnancies'] = 1
-X_test.loc[X_test['Pregnancies'] != 0, 'Pregnancies'] = 1
-
-# fit model to training data
-model = XGBClassifier(objective = 'binary:logistic', eval_metric = 'auc', use_label_encoder=False)
-model.fit(X_train, y_train)
+scores = cross_val_score(clf, X, y, cv = 5)
 
 
 # make predictions for test data
-y_pred = model.predict(X_test)
-predictions = [round(value) for value in y_pred]
+# y_pred = model.predict(X_test)
+# predictions = [round(value) for value in y_pred]
 # evaluate predictions
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+# accuracy = accuracy_score(y_test, predictions)
+print("Accuracy score before genetic algorithm: %.2f%%" % (np.mean(scores) * 100.0))
 
 # note everything in the dataset is a numerical value, just like the example below
 
@@ -57,21 +61,21 @@ print("Accuracy: %.2f%%" % (accuracy * 100.0))
 def initialization_of_population(size,n_feat):
     population = []
     for i in range(size):
-        chromosome = np.ones(n_feat, dtype=np.bool)
-        chromosome[:int(0.3*n_feat)]=False
-        np.random.shuffle(chromosome)
+        chromosome = np.ones(n_feat, dtype=np.bool) #creates bool array length of feature array
+        chromosome[:int(0.3*n_feat)]=False # creates 2 false entries
+        np.random.shuffle(chromosome) # shuffles array 
         population.append(chromosome)
     return population
 
 def fitness_score(population):
     scores = []
     for chromosome in population:
-        model.fit(X_train.iloc[:,chromosome],y_train)
-        predictions = model.predict(X_test.iloc[:,chromosome])
-        scores.append(accuracy_score(y_test,predictions))
+        cv_score = cross_val_score(clf, X.iloc[:,chromosome], y, cv = 10)
+        scores.append(np.mean(cv_score))
     scores, population = np.array(scores), np.array(population) 
     inds = np.argsort(scores)
     return list(scores[inds][::-1]), list(population[inds,:][::-1])
+
 
 def selection(pop_after_fit,n_parents):
     population_nextgen = []
@@ -99,7 +103,7 @@ def mutation(pop_after_cross, r_mut):
     #print(population_nextgen)
     return population_nextgen
 
-def generations(n_pop, n_feat, n_parents, r_mut, n_gen, X_train, X_test, y_train, y_test):
+def generations(n_pop, n_feat, n_parents, r_mut, n_gen, X, y):
     population = initialization_of_population(n_pop,n_feat)
     # keep track of best solution
     best_chromo = []
@@ -115,11 +119,13 @@ def generations(n_pop, n_feat, n_parents, r_mut, n_gen, X_train, X_test, y_train
     return best_chromo, best_score
 
 #increased n_pop, n_parents, and r_mut led to decreased AUC
-chromo, score = generations(n_pop=200, n_feat=8, n_parents = 10, r_mut=0.1,n_gen=38,X_train=X_train,X_test=X_test,y_train=y_train,y_test=y_test)
-model.fit(X_train.iloc[:,chromo[-1]],y_train)
-predictions = model.predict(X_test.iloc[:,chromo[-1]])
-print("Accuracy score after genetic algorithm is= "+str(accuracy_score(y_test,predictions)))
+# increased n_pop, n_parents and decreased r_mut led to increased AUC!
+chromo, score = generations(n_pop=400, n_feat=8, n_parents = 50, r_mut=0.001, n_gen=38, X=X, y=y)
+new_scores = cross_val_score(clf, X.iloc[:,chromo[-1]], y, cv = 10)
+print("Accuracy score after genetic algorithm is: %.2f%%" % (np.mean(new_scores)*100.00))
 
 
-
+# try to do repeated k-fold cv
+# data_dmatrix = xgb.DMatrix(data=X,label=y)
+# params = {"objective":"binary:logistic",'learning_rate': 0.1,'max_depth': 5, 'alpha': 10}
 
