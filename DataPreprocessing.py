9ed0467a033c8e4753a42069ede3c1a680d65f32
Danah Albaaj <dana.al.baaj@gmail.com>
2021-05-17 19:32:02 -0700

    finalized preprocessing

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 from sklearn.impute import KNNImputer
-from sklearn.preprocessing import Binarizer
+from sklearn.preprocessing import Binarizer, RobustScaler
 data = pd.read_csv(r'C:\Users\danah\Documents\SeniorThesis\diabetes.csv')
 data.head()
 
@@ -15,35 +15,41 @@ print('The data distribution is: ', data.groupby('Outcome').size())
 
 # the visualization of the data -- gather the understanding of the outliers and realistic values
 # need to change the size of the figures
-data.plot(kind='box', subplots = True, layout = (3,3), sharex = False, sharey = False)
-data.hist()
-plt.show()
-
-# need to encode the pregnancy column, if > 1 change value to 1
-# this can be done using binarizer!
-binarizer = Binarizer(threshold = 1)
-binarizer.transform(np.array(data.loc[:,'Pregnancies']).reshape(-1,1))
-#
-data.hist()
+colors = ['green', 'orange', 'tan', 'magenta', 'red', 'deeppink', 'purple', 'skyblue', 'royalblue']
+col = data.columns
+for i in range(len(col)): 
+    plt.hist(data.loc[:,col[i]], color=colors[i])
+    plt.title(str(col[i]))
+    plt.show()
 
 # This tells us where there are 0 values in the data
-# based on prior knowledge we can determine what 
-# is realistic and what should not be zero
+# Prior knowledge determines what is realistic and what should not be zero
 (data != 0).sum(0)
 
 
-# imputation -- replace the 0's and outliers with np.nan
+# acts like Binarizer -> set all values != 0 as 1
+data.Pregnancies = np.where(data.Pregnancies != 0, 1, data.Pregnancies)
 
+# imputation -- replace the 0's with np.nan
 columns = data.columns[1:6]
 for i in columns:
 	data.loc[:,i].replace(0, np.nan, inplace = True)
 
-# define KNN Imputer
-imputer = KNNImputer(n_neighbors = 5, weights = 'uniform', metric = 'nan_euclidean')
+# must scale data before imputation
+scaler = RobustScaler(quantile_range=(1.0, 99.0), with_centering = False)
 X = data.drop(columns = ['Outcome'])
-imputer.fit(X)
-Xtrans = imputer.transform(X)
+scaler.fit(X)
+scaledX = scaler.transform(X)
 
-# need to use KNN to impute the missing values 
-# determine what values of insulin are biologically impossible and treat those as missing and impute
-# do not discard outliers
+# define KNN Imputer
+imputer = KNNImputer(n_neighbors = 5, weights = 'distance', metric = 'nan_euclidean')
+imputer.fit(scaledX)
+Xtrans = imputer.transform(scaledX)
+
+# visualize results
+for j in range(Xtrans.shape[1]):
+    plt.hist(Xtrans[:,j], color= colors[j])
+    plt.title(str(col[j]) + ' After Imputation')
+    plt.show()
+
+
